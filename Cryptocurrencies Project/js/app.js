0c2622c0dc22246c15ee1c635ea411b50a3f2c8b
Marius Vasili <40918003+Marius-Vasili@users.noreply.github.com>
2018-12-18 11:40:27 +0100

    Update app.js

--- Cryptocurrencies Project/js/app.js
+++ Cryptocurrencies Project/js/app.js
@@ -3,14 +3,14 @@
 const cryptoAPI = new CryptoAPI();
 const ui = new UI();
 
-// Create the variables
+
 
 const form = document.getElementById('form');
 
 
 
 
-// Add event listener
+
 form.addEventListener('submit', (e) => {
      e.preventDefault();
 
@@ -20,12 +20,9 @@ form.addEventListener('submit', (e) => {
      const cryptoCurrencySelect = document.getElementById('cryptocurrency').value;
 
 
-     // Validate that the selects have something
      if(currencySelect === '' || cryptoCurrencySelect === '') {
-          // display an error
           ui.printMessage('All the fields are mandatory', 'deep-orange darken-4 card-panel');
      } else {
-          // Query the rest api
           cryptoAPI.queryAPI(currencySelect, cryptoCurrencySelect)
                .then(data => {
                     ui.displayResult( data.result[0], currencySelect );
